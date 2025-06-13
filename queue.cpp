a5857b27c89fe07044ee47a0e0f218ff3702c2b3
unknown <codus3276@kakao.com>
2025-06-13 20:40:38 +0900

    dequeue 구현

--- queue.cpp
+++ queue.cpp
@@ -61,3 +61,35 @@ Reply enqueue(Queue* queue, Item item) {
 
     return { true, {} }; // 성공적으로 작업을 마침
 }
+
+Reply dequeue(Queue* queue) {
+    std::lock_guard<std::mutex> lock(queue->mtx);
+
+    Node* sentinel = queue->head;
+    Node* nodeToRemove = sentinel->next;
+
+    // 큐가 비어있는 경우
+    if (!nodeToRemove) {
+        return { false, {} };
+    }
+
+    // 큐에서 노드를 제거
+    sentinel->next = nodeToRemove->next;
+    // 만약 마지막 노드를 제거했다면 tail 포인터를 sentinel로 되돌립니다.
+    if (!sentinel->next) {
+        queue->tail = sentinel;
+    }
+
+    // Reply 구조체에 반환할 아이템을 깊은 복사합니다.
+    Item resultItem;
+    resultItem.key = nodeToRemove->item.key;
+    resultItem.value_size = nodeToRemove->item.value_size;
+    resultItem.value = new char[resultItem.value_size];
+    memcpy(resultItem.value, nodeToRemove->item.value, resultItem.value_size);
+
+    // 원본 노드와 그 안의 value 메모리를 해제합니다.
+    delete[] static_cast<char*>(nodeToRemove->item.value);
+    delete nodeToRemove;
+
+    return { true, resultItem };
+}
